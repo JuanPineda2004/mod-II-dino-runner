9d555e14f6ea8245bd5951637071cba532ed53d7
JuanPineda2004 <126919530+JuanPineda2004@users.noreply.github.com>
2023-03-05 22:10:34 -0500

    DINO.py

--- dino_runner/components/DINO.py
+++ dino_runner/components/DINO.py
@@ -1,15 +1,19 @@
 import pygame
 from dino_runner.utils.constants import RUNNING, DUCKING, JUMPING
 from pygame.sprite import Sprite
+
 class DINOSAUR(Sprite):
     X_POS=120
     Y_POS=310
+    JUMP_VELOCITY=8.5
     def __init__(self):
         self.image=RUNNING[0]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x=self.X_POS
         self.dino_rect.y=self.Y_POS
+        self.jump_vel=self.JUMP_VELOCITY
         self.step=0
+        self.dino_jump = False
         
     
 
@@ -42,8 +46,14 @@ class DINOSAUR(Sprite):
         self.dino_rect.y=self.Y_POS+35
 
     def jump(self):
+        
         self.image = JUMPING
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x=self.X_POS
-        self.dino_rect.y=self.Y_POS-150
+        self.dino_rect.y=self.Y_POS-130
+        self.dino_rect.y -= self.jump_vel * 4
+        self.jump_vel -= 0.8
+       
+        
+        
         
