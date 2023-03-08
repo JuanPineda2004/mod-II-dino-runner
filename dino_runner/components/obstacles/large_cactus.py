61ced5c60a6eca9798908c715d683697f837ffc9
JuanPineda2004 <126919530+JuanPineda2004@users.noreply.github.com>
2023-03-07 21:34:47 -0500

    cactus

--- dino_runner/components/obstacles/large_cactus.py
+++ dino_runner/components/obstacles/large_cactus.py
@@ -2,7 +2,7 @@
 from dino_runner.components.obstacles.obstacle import Obstacle
 
 
-class SmallCactus(Obstacle):
+class LargeCactus(Obstacle):
     def __init__(self, image):
         super().__init__(image)
         self.rect.y = 325
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,16 +1,34 @@
 
+import random
+
+from dino_runner.components.obstacles.bird import Bird
+
+from dino_runner.components.obstacles.large_cactus import LargeCactus
+from dino_runner.components.obstacles.cactus import SmallCactus
 import pygame
-from dino_runner.utils.constants import SMALL_CACTUS
-from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
+
 
 
 class Obstaclemanager:
     def __init__(self):
         self.obstacles=[]
 
+    def generate_obstacle(self):
+        obstacles_types ={
+            0: SmallCactus(SMALL_CACTUS[0]),
+            1: SmallCactus(SMALL_CACTUS[1]),
+            2: SmallCactus(SMALL_CACTUS[2]),
+            3: LargeCactus(SMALL_CACTUS[0]),
+            4: LargeCactus(SMALL_CACTUS[1]),
+            5: LargeCactus(SMALL_CACTUS[2]),
+            6: Bird(BIRD[0])
+        }
+        return obstacles_types[random.randint(0, 6)]    
+
     def update(self, game):
         if len(self.obstacles)==0:
-            self.obstacles.append(Cactus(SMALL_CACTUS[0]))
+            self.obstacles.append(self.generate_obstacle())
 
 
         for obstacle in self.obstacles:
